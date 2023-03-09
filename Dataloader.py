e773a0239f3c98fbab154d52d6cee2a01c43465d
mahimoksha <kmokshagna999@gmail.com>
2023-03-08 17:18:15 -0700

    data loading done

---
+++ Dataloader.py
@@ -0,0 +1,43 @@
+from torch.utils.data import Dataset
+import pandas as pd
+import os
+from PIL import Image
+import torch
+# from skimage import io, transform
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+# import cv2
+import torchvision.transforms as transforms 
+import pickle as pk
+from PIL import Image
+import random
+
+
+
+class MonkeyPoxDataLoader(Dataset):
+    def __init__(self, csv_file, img_dir, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.monkey_pox = pd.read_csv(csv_file)
+        self.root_dir = img_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.monkey_pox)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        img_name = os.path.join(self.root_dir,self.monkey_pox.iloc[idx, 0])
+        image = np.array(Image.open(img_name))
+        label = self.monkey_pox.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
---
+++ main.py
@@ -0,0 +1,56 @@
+import numpy as np
+import torchvision
+from torchvision import transforms
+import re
+import pandas as pd
+import os
+import torch
+from sklearn.model_selection import train_test_split
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import pdb
+from sklearn.model_selection import KFold
+from torch import Tensor,optim
+import matplotlib.pyplot as plt
+from torchvision import models
+from sklearn.metrics import recall_score
+import random
+from torchvision.transforms import Normalize
+from Dataloader import MonkeyPoxDataLoader
+import warnings
+import argparse
+warnings.filterwarnings("ignore")
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")
+import time
+
+def main(args):
+    params = {'batch_size':args.batch,
+            'shuffle':True}
+    rootdir = '/home/sci/mkaranam/Desktop/ML_Project_proba/'
+    img_dir = "/home/sci/mkaranam/Desktop/ML_Project_proba/OriginalImages/OriginalImages/Total_Data/"
+    scratchDir = './Results'
+    
+    tr_csv_file = os.path.join(rootdir,"trainMonkeypox.csv")
+    cv_csv_file = os.path.join(rootdir,"cvMonkeypox.csv")
+    te_csv_file = os.path.join(rootdir,"testMonkeypox.csv")
+
+    trans = transforms.Compose([transforms.ToTensor(),Normalize(mean=(0.485), std=(0.229))])
+    test_trans = transforms.Compose([transforms.ToTensor(),Normalize(mean=(0.485), std=(0.229))])
+    train = MonkeyPoxDataLoader(tr_csv_file, img_dir, transform=trans)
+    train_dataloader = torch.utils.data.DataLoader(train, **params)
+    train_dataloader_eval = torch.utils.data.DataLoader(train, batch_size=1, shuffle=True)
+    cv = MonkeyPoxDataLoader(cv_csv_file, img_dir, transform=test_trans)
+
+    cv_dataloader = torch.utils.data.DataLoader(cv, **params)
+    cv_dataloader_eval = torch.utils.data.DataLoader(cv,batch_size=1, shuffle=True)
+
+    test = MonkeyPoxDataLoader(te_csv_file, img_dir, transform=test_trans)
+    test_dataloader = torch.utils.data.DataLoader(test, batch_size=1, shuffle=True)
+
+if __name__ == '__main__':
+        parser = argparse.ArgumentParser(description='Short sample app')
+        parser.add_argument('-batch'             ,type=int  , action="store", dest='batch'       , default=10       )
+        args = parser.parse_args()
+        main(args)
